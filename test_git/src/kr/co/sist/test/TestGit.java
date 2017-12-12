6502d9bb00888b5ed563defdcf116ed6e77c6965
owner <owner@owner-PC>
2017-12-12 12:44:40 +0900

    xzvvcv

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("GIT HUB 연습");
+		System.out.println("수정수정수정");
+		//adfdasfdasfdasfsadfdsfasd
 	}//main
 
 }//class
